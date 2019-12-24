72aa976f0792c43b8c7318882cb968ddc8f8a7b2
disckord <disckord35@hotmail.com>
2019-12-24 12:30:29 +1100

    update to dns 1.1.8
    changed protocol

--- CraftBukkit-53734d29b6b9a0bc9b2d5c66d8fff3eadb963cb8/src/main/java/net/minecraft/server/v1_5_R3/a1Version.java
+++ CraftBukkit-53734d29b6b9a0bc9b2d5c66d8fff3eadb963cb8/src/main/java/net/minecraft/server/v1_5_R3/a1Version.java
@@ -1,6 +1,6 @@
 package net.minecraft.server.v1_5_R3;
 
 public class a1Version {
-	public static final String serverVersion = "1.1.7a";
-	public static final int protocolID = -28;
+	public static final String serverVersion = "1.1.8";
+	public static final int protocolID = -29;
 }
